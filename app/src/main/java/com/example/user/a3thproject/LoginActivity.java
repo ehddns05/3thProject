3ef38c652d4a99e8bd291ccb34a59fe6175a4ff3
Dongun <user@LAPTOP-4L6BA1KH>
2017-12-28 09:38:49 +0900

    java 파일 추가

---
+++ app/src/main/java/com/example/user/a3thproject/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.user.a3thproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 101;
+
+    EditText idInput;
+    EditText passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String id = idInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                intent.putExtra("id", id);
+                intent.putExtra("password", password);
+
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+            }
+        });
+
+        idInput = (EditText) findViewById(R.id.idInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE_MENU) {
+            if (intent != null) {
+                String menu = intent.getStringExtra("menu");
+                String message = intent.getStringExtra("message");
+
+                Toast toast = Toast.makeText(getBaseContext(), "result code : " + resultCode + ", menu : " + menu + ", message : " + message, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+
+    }
+
+}
