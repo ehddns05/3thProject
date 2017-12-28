f5fc5278cc5af3f85596cfdbb0002b8a52cdfc8a
Sangyoon <ddon893@gmail.com>
2017-12-28 09:56:49 +0900

    Tactics부분 업로드합니다.

---
+++ app/src/main/java/com/example/user/a3thproject/MapTacticsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.a3thproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapTacticsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_tactics_list);
+    }
+}
---
+++ app/src/main/java/com/example/user/a3thproject/MapTaticsDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.a3thproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapTaticsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_tatics_detail);
+    }
+}
