c801c718146a5334a340a6f79924df5d05053b7e
Sangyoon <ddon893@gmail.com>
2018-01-04 17:36:52 +0900

    다른글 클릭시 기존에 열린글 자동닫기 기능 추가.

--- app/src/main/java/com/example/user/a3thproject/MapTacticsListActivity.java
+++ app/src/main/java/com/example/user/a3thproject/MapTacticsListActivity.java
@@ -2,6 +2,8 @@ package com.example.user.a3thproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -21,6 +23,7 @@ public class MapTacticsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_tactics_list);
 
@@ -29,20 +32,61 @@ public class MapTacticsListActivity extends AppCompatActivity {
 
         listAdapter = new BaseExpandableAdapter(this, parentList, childList);
         expListView.setAdapter(listAdapter);
+
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            int lastClickedPosition = -1;
+
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView,
+                                        View view, int groupPosition, long id) {
+                Log.d("클릭확인클릭확인", String.valueOf(lastClickedPosition));
+
+                if (lastClickedPosition != groupPosition) {
+                    expListView.collapseGroup(lastClickedPosition);
+                    expListView.expandGroup(groupPosition);
+                } else {
+                    expListView.collapseGroup(groupPosition);
+                }
+
+                lastClickedPosition = groupPosition;
+                return true;
+            }
+        });
     }
 
     public void preparedListData() {
         parentList = new ArrayList<>();
-        parentGroup = new TacticsParentGroup(1,"Notice", "20170102");
-
-        parentList.add(parentGroup);
-
         childList = new HashMap<>();
         innerChild = new ArrayList<>();
-        childGroup = new TacticsChildGroup(
-                "완큐의방", "박완규", "이방은탈출할수없다!!!!");
 
+        parentGroup = new TacticsParentGroup(1, "Notice1", "20170102");
+        parentList.add(parentGroup);
+        childGroup = new TacticsChildGroup(
+                "완큐의방", "박완규1", "이방은탈출할수없다!!!!");
         innerChild.add(childGroup);
         childList.put(parentGroup.getListNo(), innerChild);
+
+        parentGroup = new TacticsParentGroup(2, "Notice2", "20170102");
+        parentList.add(parentGroup);
+        childGroup = new TacticsChildGroup(
+                "완큐의방", "박완규2", "이방은탈출할수없다!!!!");
+        innerChild.add(childGroup);
+        childList.put(parentGroup.getListNo(), innerChild);
+
+        parentGroup = new TacticsParentGroup(3, "Notice3", "20170102");
+        parentList.add(parentGroup);
+        childGroup = new TacticsChildGroup(
+                "완큐의방", "박완규3", "이방은탈출할수없다!!!!");
+        innerChild.add(childGroup);
+        childList.put(parentGroup.getListNo(), innerChild);
+
+        parentGroup = new TacticsParentGroup(4, "Notice4", "20170102");
+        parentList.add(parentGroup);
+        childGroup = new TacticsChildGroup(
+                "완큐의방", "박완규4", "이방은탈출할수없다!!!!");
+        innerChild.add(childGroup);
+        childList.put(parentGroup.getListNo(), innerChild);
+
+        Log.d("innerChild의 갯수", String.valueOf(innerChild.size()));
     }
 }
